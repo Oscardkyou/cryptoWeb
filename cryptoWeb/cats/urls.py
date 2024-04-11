081774312eaf830226eb6e3c2f5a660812f5d7d7
Oscardk <dauren2050@yandex.ru>
2024-04-11 13:18:15 +0500

    Tags

--- cryptoWeb/cats/urls.py
+++ cryptoWeb/cats/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
     path('post/<int:post_id>/', views.show_post, name='post'),
+    path('category/<int:cat_id>/', views.show_category, name='category'),
     #path('dogs/<int:dog_id>/', views.categories, name='dogs_id'),
     #path('dogs/<slug:dog_slug>/', views.categories_by_slug, name='dogs'),
     #path("archive/<int:year>/", views.archive, name='archive')
